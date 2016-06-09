2093b2b08cb96247db66772b5f6e7e020cd17be9
Megan Reardon <meganr@gmail.com>
2016-06-08 17:51:33 -0700

    have sourceUrl populating properly

--- scripts/blogpost-data.js
+++ scripts/blogpost-data.js
@@ -2,32 +2,32 @@ var blogpostData = [
   {
     title:        'Cupcakes',
     author:       'Cupcake Catie',
-    sourcetitle:  'Cupcakes 4evr Blog',
-    sourceurl:    'http://www.google.com',
+    sourceTitle:  'Cupcakes 4evr Blog',
+    sourceUrl:    'http://www.google.com',
     published:    '2015-11-05',
     content:      '<p>Cupcake ipsum dolor sit. Amet cookie cookie ice cream caramels pudding bear claw. Tiramisu topping powder biscuit tiramisu jelly beans muffin chocolate. Topping biscuit tootsie roll carrot cake sweet fruitcake ice cream. Souffle sugar plum souffle candy canes caramels chocolate bar chocolate bear claw. Caramels topping sesame snaps jelly macaroon halvah. Gummi bears jujubes chocolate cake sweet jelly beans. Cake cake tart jelly-o pastry. Donut fruitcake fruitcake.</p><p>Liquorice macaroon dessert marzipan. Candy canes cookie chocolate cake sweet roll cupcake ice cream donut. Croissant lollipop powder bear claw lemon drops sweet marzipan macaroon. Lollipop danish gummi bears biscuit. Croissant dessert pie brownie ice cream. Jelly beans cheesecake bear claw sweet roll pie chocolate. Cookie toffee topping cupcake chocolate bar gummi bears powder sweet roll. Sesame snaps souffle halvah halvah tart sesame snaps.</p>'
   },
   {
     title:        'Donuts',
     author:       'Donut Donna',
-    sourcetitle:  'Donna\'s Blog All About Donuts and Donut Accessories',
-    sourceurl:    'http://www.google.com',
+    sourceTitle:  'Donna\'s Blog All About Donuts and Donut Accessories',
+    sourceUrl:    'http://www.google.com',
     published:    '2014-12-01',
     content:      '<p>Cupcake ipsum dolor sit amet macaroon. Tart ice cream chocolate cake croissant fruitcake. Muffin lemon drops apple pie sweet roll croissant chocolate bar. Apple pie candy canes muffin candy canes lollipop cake chocolate cake macaroon. Jelly icing brownie. Liquorice dragée chocolate bonbon sweet lollipop ice cream cookie. Ice cream toffee pudding toffee chocolate cake jelly beans chocolate bar jelly. Cake lollipop gingerbread dragee liquorice chocolate cake sugar plum cotton candy sweet.</p><p>Apple pie oat cake sweet roll marzipan apple pie donut. Sugar plum danish halvah marshmallow lollipop. Pastry apple pie marzipan. Muffin tart candy. Ice cream cake jelly beans toffee cake souffle. Gummies toffee cookie gingerbread caramels marshmallow sugar plum liquorice. Biscuit apple pie candy canes tiramisu muffin carrot cake soufflé cake cotton candy.</p>'
   },
   {
     title:        'Ice Cream',
     author:       'Ike Creasley',
-    sourcetitle:  'The Ice Cream Rules Blog',
-    sourceurl:    'http://www.google.com',
+    sourceTitle:  'The Ice Cream Rules Blog',
+    sourceUrl:    'http://www.google.com',
     published:    '2013-06-04',
     content:      '<p>Cupcake ipsum dolor sit amet candy caramels candy lemon drops. Croissant pudding sweet oat cake carrot cake. Gummi bears danish oat cake caramels gummi bears. Bear claw cake donut jelly-o cotton candy chocolate bar tiramisu halvah. Cupcake topping dragee icing pudding bonbon. Sugar plum liquorice cake tootsie roll dessert cake chocolate.</p><p>Liquorice souffle jujubes jelly beans pudding tart fruitcake. Cotton candy powder gingerbread cake lemon drops topping marzipan. Jelly-o biscuit sweet roll pie cupcake cookie brownie. Tootsie roll oat cake pie jelly beans tootsie roll cupcake gummies muffin chupa chups. Cotton candy fruitcake carrot cake lemon drops icing caramels cupcake. Tart icing cupcake apple pie souffle gummi bears gingerbread marzipan. Tiramisu gummies souffle cotton candy jelly cookie dragee cake tiramisu. Cupcake bear claw cupcake topping cupcake ice cream dessert bear claw. Powder sesame snaps marshmallow chocolate bar ice cream.</p>'
   },
   {
     title:        'Pie',
     author:       'Guy the Pie Guy',
-    sourcetitle:  'My Pie Blog',
-    sourceurl:    'http://www.google.com',
+    sourceTitle:  'My Pie Blog',
+    sourceUrl:    'http://www.google.com',
     published:    '2016-01-01',
     content:      '<p>Cupcake ipsum dolor sit amet croissant. Tiramisu sweet muffin gummies wafer ice cream bear claw. Toffee sweet roll wafer marshmallow icing cotton candy danish halvah lemon drops. Carrot cake liquorice icing cupcake. Biscuit gummies lollipop. Marzipan cookie chupa chups topping muffin chocolate cake ice cream cheesecake dragee. Biscuit ice cream chupa chups lollipop sugar plum lemon drops. Apple pie jelly-o souffle. Chocolate cake pastry biscuit lollipop jujubes.</p><p>Marzipan powder candy candy tiramisu dessert cake. Marshmallow bear claw candy canes cake pastry. Sweet cheesecake macaroon brownie gummi bears jelly-o. Topping danish sweet roll marzipan liquorice. Dragee dragee lemon drops icing. Toffee marshmallow chocolate bar.</p>'
   },
--- scripts/blogpost.js
+++ scripts/blogpost.js
@@ -1,12 +1,10 @@
-// all my js belongs to here
-
 var blogposts = [];
 
 function BlogPost (opts) {
   this.title = opts.title;
   this.author = opts.author;
-  this.sourcetitle = opts.sourcetitle;
-  this.sourceurl = opts.sourceurl;
+  this.sourceTitle = opts.sourceTitle;
+  this.sourceUrl = opts.sourceUrl;
   this.published = opts.published;
   this.content = opts.content;
 }
@@ -15,8 +13,8 @@ BlogPost.prototype.toHtml = function() {
   var $newBlogPost = $('article.blogpost-template').clone();
   $newBlogPost.find('.blogpost-header h2').text(this.title); // title // this could be .text instead
   $newBlogPost.find('.blogpost-header span').text(this.author); // author
-  $newBlogPost.find('.blogpost-header address').text(this.sourcetitle); // sourcetitle
-  $newBlogPost.find('.blogpost-header a').attr('href', this.sourceurl); // sourceurl
+  $newBlogPost.find('.blogpost-header a').text(this.sourceTitle); // sourceTitle
+  $newBlogPost.find('.blogpost-header a').attr('href', this.sourceUrl); // sourceUrl
   $newBlogPost.find('.blogpost-header time').text(this.published); // published
   $newBlogPost.find('.blogpost-content').html(this.content); // content // this can be html because have p tags in the string
   $newBlogPost.removeClass('blogpost-template'); // remove class from template
@@ -30,31 +28,3 @@ blogpostData.forEach(function(ele){
 blogposts.forEach(function(article){
   $('#blogposts').append(article.toHtml()); // headdesk headdesk
 });
-
-// example code below
-// ourLocalData.forEach(function(ele){
-//   articles.push(new Article(ele));
-// });
-//
-// articles.forEach(function(article){
-//   $('#articles').append(article.toHtml());
-// });
-
-
-// // example below
-// Article.prototype.toHtml = function() {
-//   // clone the whole article area
-//   var $newArticle = $('article.template').clone();
-//   $newArticle.attr('data-catagory', this.cateory); // do I need a version of this?
-//   $newArticle.find('a').html(this.author); // author
-//   $newArticle.find('a').attr('href', this.authorUrl); // author url
-//   $newArticle.find('h1').html(this.title); // title
-//   $newArticle.find('section.article-body').html(this.body); // body text
-//
-//   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-//   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-//
-//   $newArticle.removeClass('template');
-//   return $newArticle;
-// };
-// // end of example code
