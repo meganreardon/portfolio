905575528dcdb5e0e641eddb5daa0bc412f8a821
Megan Reardon <meganr@gmail.com>
2016-06-12 19:31:11 -0700

    added clearfix to blogpost images

--- scripts/blogpost-view.js
+++ scripts/blogpost-view.js
@@ -1,27 +1,17 @@
 var blogpostView = {};
 
 blogpostView.setTeasers = function() {
-  // console.log($('.blogpost-content').children());
-  // $('.blogpost-content *:nth-of-type(n+2)').hide();
   $('.blogpost-content .recipe-view').hide();
-  console.log($('.blogpost-content .recipe-view'));
   $('article').on('click', 'a.view-recipe', function(e) {
     e.preventDefault();
-    // if($(this).html() === 'View recipe  →') {
-    if($(this).html() === 'View recipe') {
-      console.log('reached inside the if statement');
-      console.log($(this).html());
+    if($(this).html() === 'View Recipe') {
       $(this).parent().find('.recipe-view').fadeIn();
-      // $(this).html('Hide recipe &larr;');
-      $(this).html('Hide recipe');
+      $(this).html('Hide Recipe');
     } else {
-      console.log('reached the else statement');
-      console.log($(this).html());
       $('body').animate({
         scrollTop: ($(this).parent().offset().top)
       },200);
-      // $(this).html('Hide recipe &larr;');
-      $(this).html('View recipe');
+      $(this).html('View Recipe');
       $(this).parent().find('.blogpost-content .recipe-view').hide();
     }
   });
