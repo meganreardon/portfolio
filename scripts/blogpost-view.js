7aee6b49e634b0dab4d1a32dfa3358826ac3d4aa
Megan Reardon <meganr@gmail.com>
2016-06-12 19:10:25 -0700

    fixed show/hide, needed ot 86 unicode

--- scripts/blogpost-view.js
+++ scripts/blogpost-view.js
@@ -1,20 +1,28 @@
 var blogpostView = {};
 
 blogpostView.setTeasers = function() {
-  console.log($('.blogpost-content').children());
-  $('.blogpost-content *:nth-of-type(n+2)').hide();
+  // console.log($('.blogpost-content').children());
+  // $('.blogpost-content *:nth-of-type(n+2)').hide();
   $('.blogpost-content .recipe-view').hide();
-  $('article').on('click', 'a.read-on', function(e) {
+  console.log($('.blogpost-content .recipe-view'));
+  $('article').on('click', 'a.view-recipe', function(e) {
     e.preventDefault();
-    if($(this).text() === 'View recipe &rarr;') {
-      $(this).parent().find('*').fadeIn();
-      $(this).html('Hide recipe &larr;');
+    // if($(this).html() === 'View recipe  →') {
+    if($(this).html() === 'View recipe') {
+      console.log('reached inside the if statement');
+      console.log($(this).html());
+      $(this).parent().find('.recipe-view').fadeIn();
+      // $(this).html('Hide recipe &larr;');
+      $(this).html('Hide recipe');
     } else {
+      console.log('reached the else statement');
+      console.log($(this).html());
       $('body').animate({
         scrollTop: ($(this).parent().offset().top)
       },200);
-      $(this).html('View recipe &rarr;');
-      $(this).parent().find('.blogpost-content *:nth-of-type(n+2)').hide();
+      // $(this).html('Hide recipe &larr;');
+      $(this).html('View recipe');
+      $(this).parent().find('.blogpost-content .recipe-view').hide();
     }
   });
 };
