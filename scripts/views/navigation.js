87631559b6292f0c0dfddbf719f62d74d8c639fa
Megan Reardon <meganr@gmail.com>
2016-06-20 15:43:15 -0700

    got rid of old handleMainNav code

--- scripts/views/navigation.js
+++ scripts/views/navigation.js
@@ -1,13 +1,15 @@
 
 
-  var whatToView = {};
+  // var whatToView = {};
+  //
+  // whatToView.handleMainNav = function() {
+  //   $('.nav-list').on('click', '.tab', function() {
+  //     $('.tab-content').hide();
+  //     $('#' + $(this).data('content')).fadeIn();
+  //   });
+  //   $('.nav-list .tab:first').click();
+  // };
+  //
+  // whatToView.handleMainNav();
 
-  whatToView.handleMainNav = function() {
-    $('.nav-list').on('click', '.tab', function() {
-      $('.tab-content').hide();
-      $('#' + $(this).data('content')).fadeIn();
-    });
-    $('.nav-list .tab:first').click();
-  };
-
-  whatToView.handleMainNav();
+  // NOTE: all old asof Class 11, replaced with about-controller and blogpost-controller and page.js info
