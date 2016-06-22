8e0f5474397d842bd796797672c16b55f12d50d0
Megan Reardon <meganr@gmail.com>
2016-06-22 13:21:13 -0700

    refactored githubrepos.js, took old github token call out of index.html

--- scripts/models/githubrepos.js
+++ scripts/models/githubrepos.js
@@ -5,23 +5,38 @@
   repos.allRepos = [];
 
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/meganreardon/repos' + '?per_page=5' + '&sort=updated',
-      type: 'GET',
-      headers: {'Authorization': 'token ' + githubToken},
-      success: function(data) {
-        console.log(data);
-        repos.allRepos = data;
-        callback();
-      }
+    $.get('/github/users/meganreardon/repos' +
+          '?per_page=10&sort=updated')
+          .done(function(data) {
+            repos.allRepos = data;
+          }).done(callback);
+  };
+
+  repos.withTheAttribute = function(attr) {
+    return repos.allRepos.filter(function(aRepo) {
+      return aRepo[attr];
     });
   };
 
-  repos.withTheAttribute = function(myAttr) {
-    return repos.allRepos.filter(function(aRepo) {
-      return aRepo[myAttr];
-    });
-  };
+  // * * * below is original code * * * //
+  // repos.requestRepos = function(callback) {
+  //   $.ajax({
+  //     url: 'https://api.github.com/users/meganreardon/repos' + '?per_page=5' + '&sort=updated',
+  //     type: 'GET',
+  //     headers: {'Authorization': 'token ' + githubToken},
+  //     success: function(data) {
+  //       console.log(data);
+  //       repos.allRepos = data;
+  //       callback();
+  //     }
+  //   });
+  // };
+  //
+  // repos.withTheAttribute = function(myAttr) {
+  //   return repos.allRepos.filter(function(aRepo) {
+  //     return aRepo[myAttr];
+  //   });
+  // };
 
   module.repos = repos;
 })(window);
