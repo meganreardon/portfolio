ceceb1703e3b32e86d8f48ac093e3359e1ee1211
Megan Reardon <meganr@gmail.com>
2016-06-23 18:52:37 -0700

    cleaned up comments in code

--- scripts/models/githubrepos.js
+++ scripts/models/githubrepos.js
@@ -18,25 +18,5 @@
     });
   };
 
-  // * * * below is original code * * * //
-  // repos.requestRepos = function(callback) {
-  //   $.ajax({
-  //     url: 'https://api.github.com/users/meganreardon/repos' + '?per_page=5' + '&sort=updated',
-  //     type: 'GET',
-  //     headers: {'Authorization': 'token ' + githubToken},
-  //     success: function(data) {
-  //       console.log(data);
-  //       repos.allRepos = data;
-  //       callback();
-  //     }
-  //   });
-  // };
-  //
-  // repos.withTheAttribute = function(myAttr) {
-  //   return repos.allRepos.filter(function(aRepo) {
-  //     return aRepo[myAttr];
-  //   });
-  // };
-
   module.repos = repos;
 })(window);
